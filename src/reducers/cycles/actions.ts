a96a36ec45f72f0b486f7b8d61f9b75af30033c9
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2023-02-10 10:57:05 -0300

    Implements immerjs

--- src/reducers/cycles/actions.ts
+++ src/reducers/cycles/actions.ts
@@ -23,6 +23,6 @@ export const markCurrentCycleAsFinishedAction = () => {
 
 export const interruptCurrentCycleAction = () => {
   return {
-    type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
+    type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
   }
 }
